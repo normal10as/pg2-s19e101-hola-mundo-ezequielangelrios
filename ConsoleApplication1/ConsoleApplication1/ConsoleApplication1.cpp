d89003a1fd9b8a5badbf262c678d3422a586afce
ezequiel rios <riosezequielangel@gmail.com>
2019-04-15 21:48:34 -0300

    hodlslsjfnjf

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,68 @@
+// ConsoleApplication1.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include "pch.h"
+#include <iostream>
+//#include "stdafx.h"
+
+using namespace std;
+
+void main(void)
+{
+	int j, i;
+	cout << "ingrese un numero:";
+	cin >> j;
+	
+	for ( i=1 ; i<11;i=i+1  )
+	{
+		cout << j*i << endl;
+
+		
+	}
+	//for anidado
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*{
+
+		int dato;
+
+		for (dato = 3; dato < 13; dato = dato + 1) {
+
+			switch (dato) {
+			case 3: cout << "El valor es tres \n";
+				break;
+			case 4: cout << "El valor es cuatro\n";
+				break;
+			case 5:
+			case 6:
+			case 7:
+			case 8: cout << "El valor esta entre 5 y 8\n";
+				break;
+			case 11: cout << "El valor es once\n";
+				break;
+
+			default: cout << "Es uno de los valores no definidos\n";
+				break;*/
+			 // fin del switch 
+		 //fin del bucle for
+	
+// si es 0 es falsa si es distinto de 0 es verdadera
+	system("pause");
+}
+
+
