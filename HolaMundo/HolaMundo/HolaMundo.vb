c2476014ed4145fba735682c04616cd3bb67a449
ezequiel rios <riosezequielangel@gmail.com>
2019-04-15 21:52:37 -0300

    hola mundo

--- HolaMundo/HolaMundo/HolaMundo.vb
+++ HolaMundo/HolaMundo/HolaMundo.vb
@@ -3,6 +3,7 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
+        Console.WriteLine("Hola mundo")
         'Console.ReadKey()
     End Sub
 End Module
